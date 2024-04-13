19023b88333dd9f4020d7e65571099af5664243b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-13 20:09:44 +0530

    some good practices😊
    const a new way of having the component, added propTypes and defaultProps

--- react4-conditional-rendering/src/UserGreeting.jsx
+++ react4-conditional-rendering/src/UserGreeting.jsx
@@ -1,10 +1,30 @@
 /* it's better if you use ternary operator */
+/* this component kinda looking robust so */
+
+import PropTypes from 'prop-types'
+
 function UserGreeting(props) {
 
- return ( props.isLoggedIn? <h2 className="welcome" > welcome {props.username} </h2> : 
-                            <h2 className="please-login"> please login to continue! </h2>);
-}  
+    const welcome = <h2 className="welcome" > 
+                       welcome {props.username} 
+                    </h2> 
+    
+    const pleaseLogin = <h2 className="please-login"> 
+                            please login to continue! 
+                        </h2>
 
+ return ( props.isLoggedIn? welcome : pleaseLogin );
+}  
+/* it's always a better practice to add 
+propTypes and defaultProps while using props */
+UserGreeting.PropTypes = {
+    isLoggedIn: Boolean,
+    username: String,
+}
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest",
+}
 export default UserGreeting
 
 
